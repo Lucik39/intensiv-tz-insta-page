ce1ec190cf8458c1535bd9fe7b2a2ab091df3545
Lucik39 <Lucik192@bk.ru>
2018-11-02 15:29:58 +0200

    Добавление слайда
    Добавляем скрипт слайд шоу

---
+++ docs/js/slideshow.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	// Переменная отвечает за активную фотографию
+	var slideIndex = 0;
+
+	showPhotos();
+
+	function showPhotos(){
+		var i;
+		var slides = $('.photo img');
+		console.log(slides);
+		console.log(slides.length); // 12
+
+		// Запускаем цикл для скрытия фотографий
+		for ( i = 0; i < slides.length; i++){
+			console.log(i);
+			$(slides[i]).hide();
+		}
+		// Считаем slideIndex переменная, которая отвечает за активные фотографии которую будем показывать
+		slideIndex++;
+		console.log(slideIndex);
+		if ( slideIndex > slides.length ) {
+			slideIndex = 1
+		}
+		$(slides[slideIndex-1]).show();
+		setTimeout(showPhotos, 2000);
+	}
+
+});

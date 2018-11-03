3f472d9f48567cfa5e1b6859d0711bbefb4ec42c
Lucik39 <Lucik192@bk.ru>
2018-11-03 17:43:39 +0200

    Выводим фотографии
    Выводим на страницу фотографии из Instagram

--- docs/js/insta.js
+++ docs/js/insta.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){ 
 
+	var userFeed = new Instafeed({
+		get: 'user',
+		userId: '8796866921',
+		accessToken: '8796866921.1677ed0.9988c90dbae243c4b1564d770e311a24',
+		resolution: 'standard_resolution',
+		limit: 60
+	});
+	userFeed.run();
+
+	setTimeout(function(){
+		$('.instafeed').addClass('shadowed');
+	}, 100);
 
 
 });

a8263f1414a4580a5d16d1da3aca5568e7012f0c
Lucik39 <Lucik192@bk.ru>
2018-11-05 09:48:59 +0200

    правки аудио
    добавление аудио трейка вверхней части сайта в шапке

--- docs/js/audio.js
+++ docs/js/audio.js
@@ -1,5 +1,44 @@
 $(document).ready(function(){
 
+	var audio = $('#audioTrack')[0];
+	console.log(audio);
 
+	$playBtn = $('#audio-play');
+	$pauseBtn = $('#audio-pause');
+	$audioControl = $('[data-type="audio-control"]');
+
+	initAudioPlayer();
+	// запуск песни через 800 милис после загрузки сайта
+	// setTimeout(initAudioPlayer, 800);
+
+	function initAudioPlayer(){
+		$playBtn.parent().show();
+		audio.loop = true;
+		// audio.play();
+
+		$audioControl.click(function(){
+			console.log('audio Control click');
+
+			if( audio.paused ) {
+				audio.play();
+				$playBtn.parent().hide();
+				$pauseBtn.parent().show();
+				console.log('paying now');
+
+			} else {
+				audio.pause();
+				$pauseBtn.parent().hide();
+				$playBtn.parent().show();
+				console.log('pause now');
+			}
+
+
+		});
+
+
+
+	}
+
+	// audio.play();
 
 });
